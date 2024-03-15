fcd286caa096931ad54486902e507c051f47655a
Ben Gibbons <17078534+axemann@users.noreply.github.com>
2024-03-14 21:06:36 -0400

    Begin adding routing table logic

--- package/unios_2.x.sh
+++ package/unios_2.x.sh
@@ -76,6 +76,19 @@ _tailscale_install() {
         echo "Done"
     fi
 
+    echo "Installing SystemD override to clean up service parameters..."
+    export TS_OVERRIDE_DIR="/etc/systemd/system/tailscaled.service.d"
+    if [ ! -d "${TS_OVERRIDE_DIR}" ]; then
+        mkdir -p "${TS_OVERRIDE_DIR}"
+    fi
+    # From https://github.com/jasonwbarnett in https://github.com/SierraSoftworks/tailscale-udm/discussions/51#discussioncomment-7912433
+    tee "${TS_OVERRIDE_DIR}/override.conf" <<- "EOF" > /dev/null
+		[Service]
+		ExecStart=
+		ExecStart=/usr/sbin/tailscaled --port=${PORT} $FLAGS
+		EOF
+
+
     echo "Restarting Tailscale daemon to detect new configuration..."
     systemctl restart tailscaled.service || {
         echo "Failed to restart Tailscale daemon"
@@ -130,13 +143,15 @@ _tailscale_routing() {
     yes_or_no "Do you wish to proceed?" && {
         case $1 in
             "enable")
-                ## Uncomment below once enough people have tested
+                ## Uncomment below once out of BETA
                 # export TAILSCALED_INTERFACE="true"
                 # sed -i "s/TAILSCALED_INTERFACE=\"[^\"]*\"/TAILSCALED_INTERFACE=\"true\"/" ${TAILSCALE_ROOT}/tailscale-env
                 export TAILSCALED_FLAGS="--socket \/var\/run\/tailscale\/tailscaled.sock --state \/data\/tailscale\/tailscaled.state"
+                _udm_set_tailnet_routes
+                _enable_ip_forwarding
                 ;;
             "disable")
-                ## Uncomment below once enough people have tested
+                ## Uncomment below once out of BETA
                 # export TAILSCALED_INTERFACE="false"
                 # sed -i "s/TAILSCALED_INTERFACE=\"[^\"]*\"/TAILSCALED_INTERFACE=\"false\"/" ${TAILSCALE_ROOT}/tailscale-env
                 export TAILSCALED_FLAGS="--state \/data\/tailscale\/tailscaled.state --tun userspace-networking"
@@ -163,7 +178,33 @@ _tailscale_routing() {
             }
         } || exit 1 && echo "No changes made to routing configuration."
     }
+
+_udm_set_tailnet_routes() {
+    # From https://github.com/FearNaBoinne in
+    # https://github.com/SierraSoftworks/tailscale-udm/discussions/51#discussioncomment-6130392,
+    # with edits
+    ROUTES=$( /sbin/ip route | /bin/grep "dev br" | /usr/bin/cut -d " " -f 1-3 )
+    echo "${ROUTES}" | while read -r route; do /sbin/ip route del ${route} table 52; done
+    echo "${ROUTES}" | while read -r route; do /sbin/ip route add ${route} table 52; done
+}
+
+_enable_ip_forwarding() {
+    # This may be needed in certain cases, per
+    # https://github.com/SierraSoftworks/tailscale-udm/discussions/51#discussioncomment-5664351 and
+    # https://tailscale.com/kb/1019/subnets/#enable-ip-forwarding
+    if [ -d /etc/sysctl.d ]; then
+        echo 'net.ipv4.ip_forward = 1' | tee -a /etc/sysctl.d/99-tailscale.conf
+        echo 'net.ipv6.conf.all.forwarding = 1' | tee -a /etc/sysctl.d/99-tailscale.conf
+        sysctl -p /etc/sysctl.d/99-tailscale.conf
+    else
+        echo 'net.ipv4.ip_forward = 1' | tee -a /etc/sysctl.conf
+        echo 'net.ipv6.conf.all.forwarding = 1' | tee -a /etc/sysctl.conf
+        sysctl -p /etc/sysctl.conf
+    fi
+}
+
 yes_or_no() {
+    # This function will go away once out of BETA
     while true; do
         read -p "$* [y/N]: " yn
         yn=${yn:-N}
